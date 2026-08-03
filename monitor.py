6fbcf5a876788e649dbb76b29c135aed47303bff
mococomo5mo5-glitch <mococomo5mo5@gmail.com>
2026-08-04 08:15:40 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,14 @@
-print("Hello USJ")
+from playwright.sync_api import sync_playwright
+
+URL = "https://store.usj.co.jp/ja/jp/store/c/extra/PCCSPRFD2A?config=true"
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    page.goto(URL, wait_until="networkidle")
+
+    print(page.title())
+
+    browser.close()
